ff973ffe85b14bcf331d1a387caa47a621b06c57
Chameleon <chameleon.work@qq.com>
2017-10-08 15:14:33 +0800

    enter

--- bin/index.js
+++ bin/index.js
@@ -4,13 +4,20 @@ import dbConf from './../config/mongodb'
 
 import logger from './../lib/common/logger'
 import mongo from './../lib/model/mongo'
-
-import server from './../lib/server'
+import process_ from './../lib/helpers/process_'
 
 logger.info('get the server configuration...')
 
-mongoose.Promise = global.Promise
+process_.findByPort(8090).then(p => {
+  if (p && p.cmd === 'node') {
+    logger.info('port has been used, cleared...')
+    process_.killByPid(p.pid).then(res => {
+      logger.info('clean up the port occupancy program')
+    })
+  }
+})
 
+mongoose.Promise = global.Promise
 mongoose.connect(dbConf.url, { useMongoClient: true })
 .then(db => {
   mongo.config.findOne({}, (err, doc) => {
@@ -19,7 +26,8 @@ mongoose.connect(dbConf.url, { useMongoClient: true })
       process.exit(1)
     } else {
       global.__conf = doc
-      server.listen(8090, '0.0.0.0')
+      logger.info('server configuration is successful!')
+      require('./../lib/server')
     }
   })
 })
